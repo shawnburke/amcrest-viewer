0f035ace21f91e5225b396ad9762bf641a1ff722
Shawn Burke <shawn@shawnburke.com>
2020-05-20 04:53:52 +0000

    basic web

--- web/server.go
+++ web/server.go
@@ -3,28 +3,23 @@ package web
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"path"
-	"sort"
 	"time"
-
-	"html/template"
 
 	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 
 	"github.com/shawnburke/amcrest-viewer/common"
-	"github.com/shawnburke/amcrest-viewer/scanner"
+	"github.com/shawnburke/amcrest-viewer/storage/data"
 )
 
-func New(args *common.Params, logger *zap.Logger) HttpServer {
+func New(args *common.Params, logger *zap.Logger, data data.Repository) HttpServer {
 
 	server := &Server{
 		FileRoot: args.DataDir,
 		Logger:   logger,
 		args:     args,
+		data:     data,
 	}
 
 	r := server.Setup("./public/")
@@ -45,6 +40,8 @@ type Server struct {
 	r        *mux.Router
 	args     *common.Params
 	server   *http.Server
+
+	data data.Repository
 }
 
 func (s *Server) Start() error {
@@ -78,79 +75,152 @@ func (s *Server) Stop() error {
 	return nil
 }
 
-func (s *Server) index(w http.ResponseWriter, r *http.Request) {
+// func (s *Server) index(w http.ResponseWriter, r *http.Request) {
 
-	var mainPageTemplate = template.Must(template.ParseFiles("index.html"))
+// 	var mainPageTemplate = template.Must(template.ParseFiles("index.html"))
 
-	files, err := scanner.FindFiles(s.FileRoot, s.Logger, true)
+// 	files, err := scanner.FindFiles(s.FileRoot, s.Logger, true)
+// 	if err != nil {
+// 		io.WriteString(w, err.Error())
+// 	}
+
+// 	sort.Slice(files, func(i, j int) bool {
+// 		return files[i].Date.Unix() > files[j].Date.Unix()
+// 	})
+
+// 	dates := make([]viewModel, 0, len(files))
+// 	for _, f := range files {
+// 		if len(f.Videos) > 0 {
+// 			dates = append(dates, newViewModel(f))
+// 		}
+// 	}
+
+// 	d := struct {
+// 		Title string
+// 		Dates []viewModel
+// 		Json  string
+// 	}{
+// 		Title: "MP4 Files",
+// 		Dates: dates,
+// 	}
+
+// 	if r.URL.Query().Get("debug") != "" {
+// 		jb, _ := json.MarshalIndent(d, "", "  ")
+
+// 		d.Json = string(jb)
+// 	}
+
+// 	err = mainPageTemplate.Execute(w, d)
+// 	if err != nil {
+// 		s.Logger.Error("Error rendering", zap.Error(err))
+// 	}
+// }
+
+// func (s *Server) serve(w http.ResponseWriter, r *http.Request) {
+
+// 	p := r.URL.Path[7:]
+
+// 	fn := path.Join(s.FileRoot, p)
+
+// 	s.Logger.Info("Request", zap.String("filename", fn),
+// 		zap.String("ip", r.RemoteAddr),
+// 		zap.String("user-agent", r.UserAgent()),
+// 	)
+// 	http.ServeFile(w, r, fn)
+// }
+
+// func (s *Server) health(w http.ResponseWriter, r *http.Request) {
+// 	// make sure the path exists
+// 	if _, err := os.Stat(s.FileRoot); err != nil {
+// 		w.WriteHeader(500)
+// 		w.Write([]byte("Bad file path"))
+// 		return
+// 	}
+
+// 	w.Write([]byte("OK"))
+// }
+
+func (s *Server) writeJson(obj interface{}, w http.ResponseWriter, status int) {
+
+	j, err := json.MarshalIndent(obj, "", "  ")
+
 	if err != nil {
-		io.WriteString(w, err.Error())
+		s.Logger.Error("Error writing JSON", zap.Error(err))
+		j = []byte(fmt.Sprintf("Error writing json for type %T: %+v", obj, obj))
 	}
 
-	sort.Slice(files, func(i, j int) bool {
-		return files[i].Date.Unix() > files[j].Date.Unix()
-	})
-
-	dates := make([]viewModel, 0, len(files))
-	for _, f := range files {
-		if len(f.Videos) > 0 {
-			dates = append(dates, newViewModel(f))
-		}
+	if status == 0 {
+		status = 200
 	}
 
-	d := struct {
-		Title string
-		Dates []viewModel
-		Json  string
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(j)
+
+}
+
+func (s *Server) writeError(err error, w http.ResponseWriter, status int) bool {
+	if err == nil {
+		return false
+	}
+
+	info := struct {
+		Message string
+		Error   string
 	}{
-		Title: "MP4 Files",
-		Dates: dates,
+		Message: "Error accessing DB",
+		Error:   err.Error(),
 	}
 
-	if r.URL.Query().Get("debug") != "" {
-		jb, _ := json.MarshalIndent(d, "", "  ")
-
-		d.Json = string(jb)
+	if status == 0 {
+		status = 500
 	}
-
-	err = mainPageTemplate.Execute(w, d)
-	if err != nil {
-		s.Logger.Error("Error rendering", zap.Error(err))
-	}
+	s.writeJson(info, w, status)
+	return true
 }
 
-func (s *Server) serve(w http.ResponseWriter, r *http.Request) {
+func (s *Server) getCamera(w http.ResponseWriter, r *http.Request) {
 
-	p := r.URL.Path[7:]
+	strID := mux.Vars(r)["id"]
 
-	fn := path.Join(s.FileRoot, p)
+	cam, err := s.data.GetCamera(strID)
 
-	s.Logger.Info("Request", zap.String("filename", fn),
-		zap.String("ip", r.RemoteAddr),
-		zap.String("user-agent", r.UserAgent()),
-	)
-	http.ServeFile(w, r, fn)
-}
+	if s.writeError(err, w, 0) {
 
-func (s *Server) health(w http.ResponseWriter, r *http.Request) {
-	// make sure the path exists
-	if _, err := os.Stat(s.FileRoot); err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte("Bad file path"))
 		return
 	}
 
-	w.Write([]byte("OK"))
+	s.writeJson(cam, w, 200)
+
+}
+
+func (s *Server) listCameras(w http.ResponseWriter, r *http.Request) {
+
+	cams, err := s.data.ListCameras()
+
+	if s.writeError(err, w, 0) {
+
+		return
+	}
+
+	s.writeJson(cams, w, 0)
+
 }
 
 func (s *Server) Setup(public string) http.Handler {
 	s.r = mux.NewRouter()
 
-	s.r.PathPrefix("/files").HandlerFunc(s.serve)
+	// assets
 	s.r.PathPrefix("/public/").Handler(
 		http.StripPrefix("/public/",
 			http.FileServer(http.Dir(public))))
-	s.r.HandleFunc("/health", s.health)
-	s.r.HandleFunc("/", s.index)
+
+	// s.r.PathPrefix("/files").HandlerFunc(s.serve)
+
+	// s.r.HandleFunc("/health", s.health)
+	// s.r.HandleFunc("/", s.index)
+
+	s.r.HandleFunc("/cameras", s.listCameras)
+	s.r.HandleFunc("/cameras/{id}", s.getCamera)
 	return s.r
 }
