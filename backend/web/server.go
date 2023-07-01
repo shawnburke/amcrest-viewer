598336d4f54618a65becc6f0a4931573747c7672
Shawn Burke <shawn@shawnburke.com>
2023-06-30 22:33:32 -0700

    fix render path

--- backend/web/server.go
+++ backend/web/server.go
@@ -188,13 +188,13 @@ func (s *Server) Setup(frontendFlutter, frontendJS string) http.Handler {
 
 	s.Logger.Info("Web server path", zap.String("flutter-path", frontendFlutter), zap.String("js-path", frontendJS))
 
+	// // website
+	// s.r.Methods("GET").PathPrefix("/js").Handler(
+	// 	http.StripPrefix("/js", http.FileServer(http.Dir(frontendJS))),
+	// )
 	// website
-	s.r.Methods("GET").PathPrefix("/js").Handler(
-		http.StripPrefix("/js", http.FileServer(http.Dir(frontendJS))),
-	)
-	// website
-	s.r.Methods("GET").PathPrefix("/flutter").Handler(
-		http.StripPrefix("/flutter", http.FileServer(http.Dir(frontendFlutter))),
+	s.r.PathPrefix("/").Handler(
+		http.FileServer(http.Dir(frontendFlutter)),
 	)
 
 	return s.r
